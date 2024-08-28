f08fe733647d99810451ae13ab659cc5b4530d60
thiago-you <thiago.youx@gmail.com>
2024-08-28 11:31:14 -0300

    remove deprecated code from decorator

--- sample/src/main/java/you/thiago/calendarvertical/sample/SampleDecorator.java
+++ sample/src/main/java/you/thiago/calendarvertical/sample/SampleDecorator.java
@@ -6,12 +6,16 @@ import android.text.style.RelativeSizeSpan;
 import you.thiago.calendarvertical.CalendarCellDecorator;
 import you.thiago.calendarvertical.CalendarCellView;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class SampleDecorator implements CalendarCellDecorator {
   @Override
   public void decorate(CalendarCellView cellView, Date date) {
-    String dateString = Integer.toString(date.getDate());
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    
+    String dateString = Integer.toString(cal.get(Calendar.DATE));
     SpannableString string = new SpannableString(dateString + "\ntitle");
     string.setSpan(new RelativeSizeSpan(0.5f), 0, dateString.length(),
         Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
